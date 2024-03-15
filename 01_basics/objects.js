1bce1636457eb1c17c9bac57530cac40dbf3ba44
Sidd3100 <siddraimb@gmail.com>
2024-03-15 20:46:29 +0530

    objects_part2

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -1,6 +1,6 @@
 mySym = Symbol("key");
 
-console.log(mySym);
+// console.log(mySym);
 
 myObject = {
     name: "Siddhartha",
@@ -21,16 +21,76 @@ myObject = {
 //All the keys in key-value pair is by default treated as a string
 //full name and myobject can only be accessed using the bracket method
 
-console.log(myObject.name);
-console.log(myObject["full name"]);
-console.log(myObject[mySym]);
-console.log(typeof myObject[mySym]);
+// console.log(myObject.name);
+// console.log(myObject["full name"]);
+// console.log(myObject[mySym]);
+// console.log(typeof myObject[mySym]);
 
 myObject.email = "123@gmail.com";
 Object.freeze(myObject);
 myObject.email = "siddraimb@gmail.com";
 console.log(myObject.email);
 
+//This was the literal method to create object
+
+//Now, given below is the singleton object
+
+// const myObj = new Object();
+
+// console.log(myObj);
+
+const tinderObject = {};
+
+tinderObject.id = "123abc";
+tinderObject.name = "Siddhartha Rai";
+tinderObject.age = 18;
+
+console.log(tinderObject);
+
+//Nested Objects are accessed using the scope operator
+
+const regularUser = {
+    email :"siddraimb@gmail.com",
+    fullname :{
+        firstname :"Siddhartha",
+        lastname :"Rai"
+    }
+}
+
+console.log(regularUser.fullname.firstname)
 
 
+
+obj1 = {1:"a",2:"b"}
+obj2 = {3:"a", 4:"b"}
+
+//Assign method
+
+const obj3 = Object.assign({},obj1,obj2);
+
+
+//spread operator
+//obj3 = {...obj1,...obj2};
+
+console.log(obj3);
+
+const user =[
+    {
+        id:"a",
+        email:"b"
+    },
+    {
+        id:"c",
+        email:"d"
+    }
+
+]
+console.log(user[0].id);
+
+console.log(tinderObject);
+console.log(Object.keys(tinderObject));
+console.log(Object.values(tinderObject));
+console.log(Object.entries(tinderObject));
+
+console.log(myObject.hasOwnProperty('isLoggedIn'));
 
