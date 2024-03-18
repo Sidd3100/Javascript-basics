f29a5d1d43211be0930b6d4b063a1d644ccf3ae6
Sidd3100 <siddraimb@gmail.com>
2024-03-18 14:39:06 +0530

    hoal done

---
+++ 01_basics/hoal.js
@@ -0,0 +1,53 @@
+//Higher Order Array Loops
+
+//for of 
+
+const num = [2,3,4,5];
+
+for (i of num){
+  //  console.log(i)
+}
+
+const greeting  = "Hello World!";
+
+for( letter of greeting){
+   // console.log(letter);
+}
+
+//Maps
+
+const map = new Map();
+
+map.set('IN', 'India')
+map.set('US', 'United States')
+map.set('UK', 'United Kingdom')
+map.set('Fr', 'France')
+
+for([key, value] of map){
+    //console.log(key , value);
+}
+
+//Objects : for of is not iterable on objects
+//For objects we use for in iteration method
+//similarly, for in iteration is not defined for maps
+const obj = {
+    name:"siddhartha",
+    batch:"F4",
+    branch:"CSE"
+}
+
+for(key in obj){
+   // console.log(key, obj[key]);
+}
+
+//Array
+
+const arr =['a','b','c','d'];
+
+for(key in arr){
+    console.log(key, arr[key]);
+}
+
+for(key of arr){
+    console.log(key);
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Javascript-basics
 basics of javascipt for react
+
+Order:
+variables
+objects
+functions
+arrow
+iife
+conditionals
